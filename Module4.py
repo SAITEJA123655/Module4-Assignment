a58031db73754d883509522471184076ff50b4c2
SAITEJA123655 <donga.saiteja2604@gmail.com>
2025-04-04 16:23:22 +0530

    Add files via upload

---
+++ Module4.py
@@ -0,0 +1,16 @@
+#Task-1
+
+# def factorial(n):
+#     if n<=1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+# num = int(input("Enter a number: "))
+# print(f"The factorial of {num} is: {factorial(num)}")
+
+#Task-2
+import math as M
+N = int(input("Enter a number:"))
+print("Square root: ",M.sqrt(N))
+print("Natural logarithm: ", M.log(N))
+print("sine: ", M.sin(N))
